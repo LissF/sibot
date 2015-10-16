7f422eb151c08eb65f86f69cecfece143945c5a5
Yegor Zatsepin <yegor@hotelscan.com>
2015-10-16 19:59:34 +0300

    Add: cache

--- src/main/java/ua/org/tenletters/sibot/BotServlet.java
+++ src/main/java/ua/org/tenletters/sibot/BotServlet.java
@@ -19,6 +19,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public final class BotServlet extends HttpServlet {
     private static final long serialVersionUID = 5L;
@@ -45,36 +49,40 @@ public final class BotServlet extends HttpServlet {
         startBot();
     }
 
-    public void destroy() {
-        // stop
+    private synchronized void startBot() {
+        if (botThread == null || !botThread.isAlive() || botThread.isInterrupted()) {
+            botThread = new Thread(new SIBot("133372591:AAHWbe8g0m6Dwxz7UZLC9DkQHM9WGSjXOZ8"));
+            botThread.start();
+        }
     }
 
-    private void startBot() {
-        botThread = new Thread(new SIBot("133372591:AAHWbe8g0m6Dwxz7UZLC9DkQHM9WGSjXOZ8"));
-        botThread.start();
-    }
-
-    private final class SIBot implements Runnable {
+    final class SIBot implements Runnable {
         private static final int MAX_ERROR_REPEAT = 3;
+
+        private static final int CACHE_MIN_SAFE_SIZE = 10;
+        private static final int CACHE_UPDATE_PORTION = 25;
 
         private final String endpoint = "https://api.telegram.org/bot";
         private final String token;
 
+        private final Queue<String> cache = new ConcurrentLinkedQueue<>();
+        private volatile boolean cacheIsLocked = false;
+
         private String lastError = "";
         private int errorCounter = 0;
 
-        public SIBot(String token) {
+        public SIBot(final String token) {
             this.token = token;
-            System.out.println("[TELEGRAM] Created Bot");
+            System.out.println("[SIBOT] Created Bot");
         }
 
-        public HttpResponse<JsonNode> sendMessage(Integer chatId, String text) throws UnirestException {
-            System.out.println("[TELEGRAM] Sending message");
+        public HttpResponse<JsonNode> sendMessage(final Integer chatId, final String text) throws UnirestException {
+            System.out.println("[SIBOT] Sending message");
             return Unirest.post(endpoint + token + "/sendMessage").field("chat_id", chatId).field("text", text)
                     .asJson();
         }
 
-        public HttpResponse<JsonNode> getUpdates(Integer offset) throws UnirestException {
+        public HttpResponse<JsonNode> getUpdates(final Integer offset) throws UnirestException {
             return Unirest.post(endpoint + token + "/getUpdates").field("offset", offset).asJson();
         }
 
@@ -84,7 +92,7 @@ public final class BotServlet extends HttpServlet {
             } catch (Exception e) {
                 errorMessageOnFace = e.toString();
 
-                System.out.println("[TELEGRAM] Error: " + errorMessageOnFace);
+                System.out.println("[SIBOT] Error: " + errorMessageOnFace);
                 if (MAX_ERROR_REPEAT < errorCounter) {
                     if (lastError.equals(errorMessageOnFace)) {
                         ++errorCounter;
@@ -98,8 +106,8 @@ public final class BotServlet extends HttpServlet {
         }
 
         public void runBotLoop() throws UnirestException {
-            System.out.println("[TELEGRAM] Starting loop");
-            int last_update_id = 0; // last processed command
+            System.out.println("[SIBOT] Starting loop");
+            int last_update_id = 0;
 
             HttpResponse<JsonNode> response;
             while (true) {
@@ -113,20 +121,24 @@ public final class BotServlet extends HttpServlet {
                         last_update_id = responses.getJSONObject(responses.length() - 1).getInt("update_id") + 1;
                     }
 
-                    System.out.println("[TELEGRAM] Got something");
+                    System.out.println("[SIBOT] Got something");
                     for (int i = 0; i < responses.length(); ++i) {
-                        final JSONObject message = responses.getJSONObject(i).getJSONObject("message");
+                        final JSONObject message = responses.getJSONObject(i).optJSONObject("message");
+                        if (message == null) {
+                            continue;
+                        }
                         final int chatId = message.getJSONObject("chat").getInt("id");
                         final String text = message.optString("text", "");
 
-                        if (text.contains("/start")) {
-                            final String reply = "Версия бота: 1.0\n/ask - случайная тема свояка из базы db.chgk.info";
-                            sendMessage(chatId, reply);
-                        } else if (text.contains("/ask")) {
+                        if (!text.isEmpty()) {
                             new Thread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    sendRandomQuestions(chatId, 1);
+                                    if (text.contains("/start")) {
+                                        onStartCommand(chatId);
+                                    } else if (text.contains("/ask")) {
+                                        onAskCommand(chatId);
+                                    }
                                 }
                             }).start();
                         }
@@ -135,23 +147,57 @@ public final class BotServlet extends HttpServlet {
             }
         }
 
-        private void sendRandomQuestions(final int chatId, final int amount) {
+        private void onStartCommand(final int chatId) {
             try {
-                final Document doc = Jsoup.connect("http://db.chgk.info/random/from_2000-01-01/answers/types5/limit"
-                        + amount).get();
-                final Elements questions = doc.getElementsByClass("random_question");
-                for (Element question : questions) {
-                    sendMessage(
-                            chatId,
-                            Jsoup.clean(question.toString(), "", Whitelist.none(),
-                                    new Document.OutputSettings().prettyPrint(false)).replace("&nbsp;", "")
-                    );
+                final String reply = "Версия бота: 1.0\n/ask - случайная тема свояка из базы db.chgk.info";
+                sendMessage(chatId, reply);
+            } catch (UnirestException e) {
+                System.out.println("[SIBOT] Can not send question! " + e.toString());
+            }
+        }
+
+        private void onAskCommand(final int chatId) {
+            try {
+                sendMessage(chatId, cache.isEmpty() ? getRandomTheme() : cache.poll());
+                if (!cacheIsLocked && CACHE_MIN_SAFE_SIZE > cache.size()) {
+                    updateCache(CACHE_UPDATE_PORTION);
                 }
             } catch (IOException e) {
-                System.out.println("[TELEGRAM] Can not get question! " + e.toString());
+                System.out.println("[SIBOT] Can not get question! " + e.toString());
             } catch (UnirestException e) {
-                System.out.println("[TELEGRAM] Can not send question! " + e.toString());
+                System.out.println("[SIBOT] Can not send question! " + e.toString());
             }
+        }
+
+        private String getRandomTheme() throws IOException {
+            final Document doc = Jsoup.connect("http://db.chgk.info/random/from_2000-01-01/types5/limit1").get();
+            final Elements questions = doc.getElementsByClass("random_question");
+            final StringBuilder message = new StringBuilder();
+            if (questions.isEmpty()) {
+                System.out.println("[SIBOT] Can not parse question!");
+            } else {
+                message.append(Jsoup.clean(questions.get(0).toString(), "", Whitelist.none(),
+                        new Document.OutputSettings().prettyPrint(false)).replace("&nbsp;", ""));
+            }
+            return message.toString();
+        }
+
+        private synchronized void updateCache(final int themesToLoad) throws IOException {
+            cacheIsLocked = true;
+            final Document doc = Jsoup.connect("http://db.chgk.info/random/from_2000-01-01/types5/limit"
+                    + themesToLoad).get();
+            final Elements questions = doc.getElementsByClass("random_question");
+            if (questions.isEmpty()) {
+                System.out.println("[SIBOT] Can not parse question!");
+            } else {
+                final List<String> temp = new LinkedList<>();
+                for (Element question : questions) {
+                    temp.add(Jsoup.clean(question.toString(), "", Whitelist.none(),
+                            new Document.OutputSettings().prettyPrint(false)).replace("&nbsp;", "").trim());
+                }
+                cache.addAll(temp);
+            }
+            cacheIsLocked = false;
         }
     }
 }
